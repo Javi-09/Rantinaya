736569b6fdb5c6d71b4d6b82bc883c963c88a304
genesisortega21 <areliislucio2002@gmail.com>
2024-02-02 00:35:13 -0500

    Prueba

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.e_commerce"
-        minSdk = 27
+        minSdk = 24
         targetSdk = 34  // Actualizado a la última versión
         versionCode = 1
         versionName = "1.0"
